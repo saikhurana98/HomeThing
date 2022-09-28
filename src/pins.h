d55917308c7f4dffdb54436309230a47a62d2cfd
Sai Khurana <44598300+saikhurana98@users.noreply.github.com>
2022-09-28 10:48:55 +0530

    initial commit with basic lcd and rgb code

---
+++ src/pins.h
@@ -0,0 +1,25 @@
+// LCD Display
+#define LCD_MISO 19
+#define LCD_MOSI 23
+#define LCD_SCK 18
+#define LCD_CS 5
+#define LCD_RS 4
+#define LCD_RST 2
+
+// Encoder
+
+
+// RGB LED
+#define RGB 13
+#define NUM_LEDS 3
+
+// Button
+
+
+// DHT 11
+
+
+// Buzzer
+
+
+// IR Transmitter
